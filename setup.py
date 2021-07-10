e21f9edc120e1300d29aa6f5b90d48634608ec4d
Klemen Tušar <techouse@gmail.com>
2021-07-10 10:22:41 +0100

    :pushpin: Pin python-tabulate to <0.8.6 for Python 3.4 or less

--- setup.py
+++ setup.py
@@ -17,7 +17,8 @@ requires = [
     "simplejson>=3.16.0",
     "tqdm>=4.35.0",
     "packaging>=20.3",
-    "tabulate",
+    "tabulate<0.8.6 ; python_version<'3.5'",
+    "tabulate ; python_version>='3.5'",
 ]
 
 about = {}
